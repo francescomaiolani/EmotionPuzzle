ad1801f0ea816cba9d72a5d565a0b847896bbb6b
Alessandro <alessandro.rotili@gmail.com>
2018-11-09 18:34:08 +0100

    Aggiunta pagina di login e database sqlite

---
+++ Assets/Scripts/Database/DatabaseManager.cs
@@ -0,0 +1,79 @@
+﻿using Mono.Data.Sqlite;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class DatabaseManager: MonoBehaviour {
+
+    //Metodo utilizzato per inserire un nuovo account nel database
+    public static bool InsertTherapist(string username, string password)
+    {
+        //Controlla se il nome è già inserito
+        if (CheckIfAlreadyExist(username))
+            return false;
+
+        //Effettuiamo la query di inserimento
+        string connectionString = "URI=file:" + Application.dataPath + "/EmotionPuzzleDB.s3db"; //Path to database
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            //Open connection to the database
+            dbConnection.Open();
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            string sqlQuery = string.Format("INSERT INTO Therapists (Username, Password) VALUES (\"{0}\", \"{1}\")", username, password);
+            dbCommand.CommandText = sqlQuery;
+            dbCommand.ExecuteScalar();
+            dbConnection.Close();
+        }
+        return true;
+    }
+
+    //Metodo che vede se nel database è presente uno username con una data password
+    public static bool GetTherapist(string username, string password)
+    {
+        bool flag = false;
+        string connectionString = "URI=file:" + Application.dataPath + "/EmotionPuzzleDB.s3db"; //Path to database
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            //Open connection to the database
+            dbConnection.Open();
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            string sqlQuery = string.Format("SELECT * FROM Therapists WHERE username = (\"{0}\") AND password = (\"{1}\")", username, password);
+            dbCommand.CommandText = sqlQuery;
+            IDataReader reader = dbCommand.ExecuteReader();
+
+            if (reader.Read())
+            {
+                Debug.Log("Ho trovato " + reader.GetString(0) + " " + reader.GetString(1));
+                flag = true;
+            }
+            dbConnection.Close();
+        }
+        return flag;
+    }
+    
+    //Metodo utilizzato per vedere se nel database è gia presente un certo username: return true se il nome esiste già
+    public static bool CheckIfAlreadyExist(string username)
+    {
+        bool flag = false ;
+        string connectionString = "URI=file:" + Application.dataPath + "/EmotionPuzzleDB.s3db"; //Path to database
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            //Open connection to the database
+            dbConnection.Open();
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            string sqlQuery = string.Format("SELECT * FROM Therapists WHERE Username = (\"{0}\")", username);
+            dbCommand.CommandText = sqlQuery;
+            IDataReader reader = dbCommand.ExecuteReader();
+            if (reader.Read())
+            {
+                flag = true;
+            }
+            dbConnection.Close();
+        }
+        return flag;
+    }
+
+
+}
---
+++ Assets/Scripts/Menu/CredentialsManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CredentialsManager : MonoBehaviour {
+
+    public Text loginButtonText;
+    public Text signupButtonText;
+    public Text mainButtonText;
+    public Text errorMessage;
+    public GameObject errorPanel;
+    public InputField usernameField;
+    public InputField passwordField;
+    public InputField confirmPasswordField;
+
+ 	public void SignupButtonClick()
+    {
+        errorPanel.SetActive(false);
+        ClearForm();
+        confirmPasswordField.gameObject.SetActive(true);
+        mainButtonText.text = "REGISTRATI";
+    }
+
+    public void LoginButtonClick()
+    {
+        errorPanel.SetActive(false);
+        ClearForm();
+        confirmPasswordField.gameObject.SetActive(false);
+        mainButtonText.text = "ENTRA";
+    }
+
+    public void MainButtonClick()
+    {
+        //Controllo sul riempimento completo del form
+        if (!CheckFormValidity())
+        {
+            errorMessage.text = "Controlla di aver riempito tutti i campi";
+            errorPanel.SetActive(true);
+            return;
+        }
+        
+        if (mainButtonText.text == "ENTRA")
+        {
+            if (DatabaseManager.GetTherapist(usernameField.text, passwordField.text))
+            {
+                SceneManager.LoadScene("MinigameSelection");
+            } else
+            {
+                errorMessage.text = "Lo username e/o la password sono sbagliati";
+                errorPanel.SetActive(true);
+                return;
+            }
+        }
+        else
+        {
+            if (!PasswordConfirmCheck())
+            {
+                errorMessage.text = "Controlla di aver inserito correttamente entrambe le password";
+                errorPanel.SetActive(true);
+                return;
+            }
+
+            //Se l'inserimento è andato a buon fine allora andiamo nella selezione dei minigame altrimenti mandiamo un messaggio di errore
+            if (DatabaseManager.InsertTherapist(usernameField.text, passwordField.text))
+            {
+                SceneManager.LoadScene("MinigameSelection");
+            }
+            else
+            {
+                errorMessage.text = "Non è stato possibile effetuare la registrazione.";
+                errorPanel.SetActive(true);
+                return;
+            }
+        }
+    }
+
+    //Metodo utilizzato per verificare che il form Password e Confirm password combacino
+    private bool PasswordConfirmCheck()
+    {
+        if (passwordField.text != confirmPasswordField.text)
+            return false;
+        return true;
+    }
+
+    //Metodo utilizzato per verificare che tutti i campi del form sono stati riempiti
+    private bool CheckFormValidity()
+    {
+        if (usernameField.text == "")
+            return false;
+        if (passwordField.text == "")
+            return false;
+        if (mainButtonText.text == "REGISTRATI")
+            if (confirmPasswordField.text == "")
+                return false;      
+        return true;
+    }
+
+    private void ClearForm()
+    {
+        usernameField.text = "";
+        passwordField.text = "";
+        confirmPasswordField.text = "";
+    }
+}
