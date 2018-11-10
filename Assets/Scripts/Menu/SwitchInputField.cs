dee4bba0c561ca934acefca841939f17f3337469
Alessandro <alessandro.rotili@gmail.com>
2018-11-10 12:21:22 +0100

    Switch tra inputField con il tasto TAB

---
+++ Assets/Scripts/Menu/SwitchInputField.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SwitchInputField : MonoBehaviour {
+
+    public GameObject[] inputFields;
+
+    void Update()
+    {
+        SwitchWithTab();
+    }
+
+    private void SwitchWithTab()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (inputFields[0].GetComponent<InputField>().isFocused)
+            {
+                inputFields[1].GetComponent<InputField>().Select();
+                return;
+            }
+
+            if (inputFields[1].GetComponent<InputField>().isFocused)
+            {
+                if (inputFields[2].activeInHierarchy)
+                    inputFields[2].GetComponent<InputField>().Select();
+                else
+                    inputFields[0].GetComponent<InputField>().Select();
+                return;
+            }
+            if (inputFields[2].GetComponent<InputField>().isFocused && inputFields[2].activeInHierarchy)
+            {
+                inputFields[0].GetComponent<InputField>().Select();
+                return;
+            }
+
+        }
+    }
+}
