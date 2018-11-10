df420cc012e6c8b9f752228d6d15588e1f87a506
Alessandro <alessandro.rotili@gmail.com>
2018-11-10 12:27:26 +0100

    Modifiche allo script SwitchInputField

--- Assets/Scripts/Menu/SwitchInputField.cs
+++ Assets/Scripts/Menu/SwitchInputField.cs
@@ -6,34 +6,35 @@ using UnityEngine.UI;
 
 public class SwitchInputField : MonoBehaviour {
 
-    public GameObject[] inputFields;
+    public InputField[] inputFields;
 
     void Update()
     {
         SwitchWithTab();
     }
 
+    //Metodo che permete di andare da un InputField all'altro con il tasto TAB
     private void SwitchWithTab()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            if (inputFields[0].GetComponent<InputField>().isFocused)
+            if (inputFields[0].isFocused)
             {
-                inputFields[1].GetComponent<InputField>().Select();
+                inputFields[1].Select();
                 return;
             }
 
             if (inputFields[1].GetComponent<InputField>().isFocused)
             {
-                if (inputFields[2].activeInHierarchy)
-                    inputFields[2].GetComponent<InputField>().Select();
+                if (inputFields[2].IsActive())
+                    inputFields[2].Select();
                 else
-                    inputFields[0].GetComponent<InputField>().Select();
+                    inputFields[0].Select();
                 return;
             }
-            if (inputFields[2].GetComponent<InputField>().isFocused && inputFields[2].activeInHierarchy)
+            if (inputFields[2].isFocused && inputFields[2].IsActive())
             {
-                inputFields[0].GetComponent<InputField>().Select();
+                inputFields[0].Select();
                 return;
             }
 
