47dea2a4909ef6204425545072d48eb08b340b4f
Alessandro <alessandro.rotili@gmail.com>
2018-12-10 12:27:58 +0100

    Alcuni cambiamenti UI

--- Assets/Scripts/UI/UIHowDoYouFeelManager.cs
+++ Assets/Scripts/UI/UIHowDoYouFeelManager.cs
@@ -6,18 +6,15 @@ public class UIHowDoYouFeelManager : UIEndRoundManager
 {
     protected override void SetQA(bool roundResult)
     {
-        imagesQA[0].sprite = Resources.Load<Sprite>("Sprite/CompleteFaces/face" + gameManager.GetEmotionString());
+        SpawnFace(new Vector3(0, 0, 0), gameManager.GetComponent<MinigameManager>().GetMainEmotion(), gameManager.GetComponent<MinigameManager>().GetMainEmotion(), true, 1.2f);
         if (roundResult)
         {
-            sentencesQA[0].text = gameManager.GetEmotionAnswerString();
-            sentencesQA[0].gameObject.SetActive(true);
+            sentencesQA[0].text = gameManager.ConvertInCorrectText(gameManager.GetEmotionString());
         }
         else
         {
-            sentencesQA[1].text = gameManager.GetEmotionAnswerString();
-            sentencesQA[1].gameObject.SetActive(true);
-            sentencesQA[2].text = gameManager.GetEmotionString();
-            sentencesQA[2].gameObject.SetActive(true);
+            sentencesQA[1].text = gameManager.ConvertInCorrectText(gameManager.GetEmotionString());
+            sentencesQA[2].text = gameManager.ConvertInCorrectText(gameManager.GetEmotionAnswerString());
         }
     }
 }
