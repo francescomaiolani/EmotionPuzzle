6409169036db67a44c7870a0bbee9aaa27c9d039
Alessandro <alessandro.rotili@gmail.com>
2019-01-27 18:31:30 +0100

    Introducendo la data visualization

---
+++ Assets/Scripts/DataVisualization/Diagram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diagram : MonoBehaviour {
+
+    //Inserisci quanti errori sono stati fatti per emozione
+    public int[] errorValues;
+
+    public GameObject[] rects;
+
+    public float maxSize;
+
+    private int totalErrors;
+
+    void Start()
+    {
+        totalErrors = GetTotalErrors();
+        SetRectValues();
+    }
+
+    private void SetRectValues()
+    {
+        float perc;
+        float value;
+
+        if (errorValues.Length != rects.Length)
+            Debug.LogError("Numero differente tra rettangoli e valori");
+        for (int i = 0; i < errorValues.Length; i++)
+        {
+            value = (errorValues[i] * maxSize) / totalErrors;
+            rects[i].transform.localScale = new Vector3(rects[i].transform.localScale.x, value, rects[i].transform.localScale.z);
+        }
+    }
+
+    private int GetTotalErrors()
+    {
+        int sum = 0;
+        foreach(int v in errorValues)
+        {
+            sum += v;
+        }
+        return sum;
+    }
+}
